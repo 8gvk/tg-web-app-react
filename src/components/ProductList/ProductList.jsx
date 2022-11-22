ed5bbd9909c1e6726032c26612af73958c2a7e5c
vvk89 <klyuchnikvitaly@yandex.ru>
2022-11-23 02:24:01 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -8,7 +8,7 @@ const products = [
     {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
     {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
     {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 12200, description: 'Зеленого цвета, теплая'},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
     {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
     {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
     {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
@@ -40,6 +40,13 @@ const ProductList = () => {
         })
     }, [addedItems])
 
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
         let newItems = [];
@@ -52,31 +59,15 @@ const ProductList = () => {
 
         setAddedItems(newItems)
 
-
-    }
-
-    useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData)
-        return () => {
-            tg.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData])
-
-    useEffect(()=> {
-        if (newItems.length === 0) {
-            tg.MainButton.disable();
+        if(newItems.length === 0) {
+            tg.MainButton.hide();
         } else {
-            tg.MainButton.enable();
+            tg.MainButton.show();
             tg.MainButton.setParams({
                 text: `Купить ${getTotalPrice(newItems)}`
             })
         }
-    })
-
-
-
-
-
+    }
 
     return (
         <div className={'list'}>
