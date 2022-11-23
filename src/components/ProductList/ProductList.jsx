3fa808c8285e31437a9dc86dbbd336e8fd8bff8b
vvk89 <klyuchnikvitaly@yandex.ru>
2022-11-23 03:08:25 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -59,17 +59,15 @@ const ProductList = () => {
 
         setAddedItems(newItems)
 
-        useEffect(()=>{
-            if(newItems.length === 0) {
-                tg.MainButton.hide();
-            } else {
-                tg.MainButton.show();
-                tg.MainButton.setParams({
-                    text: `Купить ${getTotalPrice(newItems)}`
-                })
-            }
-        },[newItems,getTotalPrice()])
 
+        if(newItems.length === 0) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
     }
 
     return (
